df578f50a72b6f2e8995e588eb202e184a7ed0c0
Priyam-2787 <priyampreyashi@gmail.com>
2024-09-18 14:17:52 +0200

    Added put and delete method

--- paxx media/Controllers/UsersController.cs
+++ paxx media/Controllers/UsersController.cs
@@ -20,7 +20,7 @@ namespace paxx_media.Controllers
 
         }
 
-        // Get Method - Select/Read data
+        // Get :api/users
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
@@ -42,6 +42,78 @@ namespace paxx_media.Controllers
 
             return user;
         }
+
+        // POST: api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _datacontext.Users.Add(user);
+            await _datacontext.SaveChangesAsync();
+
+            // Return the newly created user along with the route to access it
+            return CreatedAtAction(nameof(GetUser), new { id = user.ID }, user);
+        }
+
+        // PUT: api/users/{id}
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.ID)
+            {
+                return BadRequest();
+            }
+
+            _datacontext.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _datacontext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _datacontext.Users.Any(e => e.ID== id);
+
+        }
+
+        // DELETE: api/users/{id}
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _datacontext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _datacontext.Users.Remove(user);
+            await _datacontext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool user(int id)
+        {
+            return _datacontext.Users.Any(e => e.ID == id);
+        }
+
+
 
 
     }
